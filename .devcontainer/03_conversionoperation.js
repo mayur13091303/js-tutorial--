696f58c2354508a2d7ab7b9416028701a3231c8f
mayur13091303 <146485549+mayur13091303@users.noreply.github.com>
2023-09-29 21:29:11 +0000

    convesion

--- .devcontainer/03_conversionoperation.js
+++ .devcontainer/03_conversionoperation.js
@@ -1,24 +1,47 @@
-let score="mayur"
+// let score="mayur"
 
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-// "33" =>33
-// "33abc"=>Nan
-// true=>1
-// false=>0
-// ""=>false
-// "mayur"=>true
+// // "33" =>33
+// // "33abc"=>Nan
+// // true=>1
+// // false=>0
+// // ""=>false
+// // "mayur"=>true
 
-let isloggedin = 0
+// let isloggedin = 0
 
-let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin);
+// let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
 
-let number=32
-let stringconvert =String(number)
-console.log(stringconvert);
+// let number=32
+// let stringconvert =String(number)
+// console.log(stringconvert);
+
+// *******************************************Operations**************************************//
+
+// let value=3
+// let negvalue = -value
+// console.log(negvalue);
+
+// let str1="hello "
+// let str2="Mayuresh"
+
+// let str3= str1 + str2
+// console.log(str3);
+
+// console.log(true);
+
+
+// let num1,num2,num3
+// num1=num2=num3=4
+// console.log(num1);
+
+let gamecounter =100
+gamecounter++;
+console.log(gamecounter);
