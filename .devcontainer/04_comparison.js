239bb07f6612d2ee976b1ec97835d7cfcf7687dc
mayur13091303 <146485549+mayur13091303@users.noreply.github.com>
2023-09-29 21:37:49 +0000

    comparison

---
+++ .devcontainer/04_comparison.js
@@ -0,0 +1,11 @@
+console.log("2">1);
+console.log(2<=1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0)
+console.log(undefined >0);
+
+// comparison and equalty check is different //
+
+// ===
+console.log("2"===2);
