56fce9b11d7621a5a3338a9b76d38ee4d5a4c1aa
mayur13091303 <146485549+mayur13091303@users.noreply.github.com>
2023-09-29 20:51:03 +0000

    da

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js codes as newer version
+
+// alert("Hello")// we are using nodejs,not browser 
+let name="Mayuresh"
+let age=18
+let islogin =true
+let state=null
+
+// [primitive datatypes]
+// number=> 2 to power 53
+// bigint
+// string=>""
+ //Boolean=>True/False
+// null => standalone value
+// undefined=>  
+// symbol => unique 
+
+// object
+
+console.log(typeof null); //type of null is object
