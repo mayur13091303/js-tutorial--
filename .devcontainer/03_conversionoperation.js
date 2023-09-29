852c228921712c92e5323f1b1edc30a6ba07f315
mayur13091303 <146485549+mayur13091303@users.noreply.github.com>
2023-09-29 21:11:38 +0000

    conver

---
+++ .devcontainer/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score="mayur"
+
+
+console.log(typeof score);
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" =>33
+// "33abc"=>Nan
+// true=>1
+// false=>0
+// ""=>false
+// "mayur"=>true
+
+let isloggedin = 0
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+let number=32
+let stringconvert =String(number)
+console.log(stringconvert);
