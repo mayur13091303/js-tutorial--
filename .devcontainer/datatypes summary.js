9b9e71ea46278e3e88ca45dd64563232d5048438
mayur13091303 <146485549+mayur13091303@users.noreply.github.com>
2023-09-30 07:35:03 +0000

    datatype

---
+++ .devcontainer/datatypes summary.js
@@ -0,0 +1,39 @@
+// Primitive datatype =>call by value //  7 categories
+// => string, number, boolean, null,undefined,Symbol,BigInt
+
+const score= 2011
+const scorevalue = 102.34
+const islogin = true
+const temp = null
+
+const id = Symbol("123")
+const anotherid = Symbol("123")
+console.log(id==anotherid);
+
+
+// =>BigInt
+  const BIGNUMBER = 24453453545375n  
+
+
+
+
+// NOn-Primitive datatype => //
+// Array, Objects, Functions
+
+// array
+const heroes =["MAngo","banana","grapes"]  
+
+// object
+let obj ={
+    name:"MAyuresh",
+    age:19
+}
+
+// Function 
+
+const myfunc=function (){
+    console.log("Hello Mayur");
+}
+
+
+console.log(typeof BIGNUMBER );
